c5c0afaa5c67e0e8241ac54107e39c78097101e2
guyko91 <guyko91@gmail.com>
2021-03-13 23:28:56 +0900

    1차 수정

--- AddressConverter/addr.sh
+++ AddressConverter/addr.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+rm -rf ./result.txt
+
+TOTAL_COUNT=`cat test.txt | wc -l`
+COUNT=1
 
 cat test.txt | while read line
 do
-	echo $line
+	API=`curl -s \
+    		--data-urlencode "resultType=json" \
+    		--data-urlencode "currentPage=1" \
+    		--data-urlencode "countPerPage=1" \
+    		--data-urlencode "keyword=$line" \
+    		--data-urlencode "confmKey=U01TX0FVVEgyMDIwMDYwNDExMzEwOTEwOTgyODk=" \
+	    	http://www.juso.go.kr/addrlink/addrLinkApi.do`
+	echo $API | jq -r '.results.juso[0].jibunAddr' >> result.txt
+
+	echo $COUNT / $TOTAL_COUNT
+	COUNT=$((COUNT+1))
 done
